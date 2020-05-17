fd59aca6bdf9f89473cf84e2a23872284c1f0f86
sakamoto-t-gk <65432135+sakamoto-t-gk@users.noreply.github.com>
2020-05-17 13:05:12 +0900

    Revert "Update script.js"
    This reverts commit 1148673f4e980f2f44eb0f06955bd86603784c16.

--- script.js
+++ script.js
@@ -44,9 +44,8 @@ window.__SKYWAY_KEY__ = '6b1e337e-0e14-46e7-8fc2-44af3bb36d8b';
 
     const mediaConnection = peer.call(remoteId.value, localStream,{
       videoCodec: 'VP8',
-      audioCodec: 'Opus',
-      videoBandwidth: 100,
-      audioBandwidth: 510
+      audioCodec: 'G.722',
+      videoBandwidth: 100
     });
 
     mediaConnection.on('stream', async stream => {
@@ -71,9 +70,8 @@ window.__SKYWAY_KEY__ = '6b1e337e-0e14-46e7-8fc2-44af3bb36d8b';
 
     const answerOption = {
       videoCodec: 'VP8',
-      audioCodec: 'Opus',
-      videoBandwidth: 100,
-      audioBandwidth: 510
+      audioCodec: 'G.722',
+      videoBandwidth: 100
     };
 
     mediaConnection.answer(localStream, answerOption);
