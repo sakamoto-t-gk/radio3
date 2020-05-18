d31944aed7a29a207465b4781a4575934c023aa3
sakamoto-t-gk <65432135+sakamoto-t-gk@users.noreply.github.com>
2020-05-18 13:57:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -83,13 +83,25 @@ toggleMicrophone.addEventListener('click', () => {
   });
 
 //  peer.once('open', id => (localId.textContent = id));
+//--------
   peer.once('open', id => {
     localId.textContent = id;
+
+peer.listAllPeers(function(list){
+
+list.forEach(function(e, i) {
+var sl = document.getElementById("sel1");
+var opt = document.createElement('option');
+opt.setAttribute('value', i);
+opt.innerHTML = e;
+sl.appendChild(opt);
+});
+
+});
     
   });
+//--------
 
-//--------
-//--------
 
 
   // Register callee handler
